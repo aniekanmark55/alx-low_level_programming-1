5ead333cecc9293ecac8be35b9fcbfdc7bae7353
Esere Cynthia <113634755+Roys77111@users.noreply.github.com>
2022-10-14 17:16:03 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,13 +18,13 @@ while (e < 58)
 i = 48;
 while (i < 58)
 {
-g = 48;										
+g = 48;
 while (g < 58)
 {
 if (e != i && e != g && i != g && e < i && i < g)
 {
 putchar(e);
-putchar(i);									
+putchar(i);
 putchar(g);
 if (i == 56 && e == 55 && g == 57)
 {
