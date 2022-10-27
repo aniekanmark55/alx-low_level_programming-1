16adb925272cff2a94d4768d869b6bbdb049d69f
Esere Cynthia <113634755+Roys77111@users.noreply.github.com>
2022-10-27 18:53:00 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * rot13 - encodes a string into rot13
- * @s: string to encode
- *
- * Return: address of s
- */
+* rot13 - encodes a string into rot13
+* @s: string to encode
+*
+* Return: address of s
+*/
 char *rot13(char *s)
 {
 	int i, j;
@@ -18,10 +18,10 @@ char *rot13(char *s)
 		for (j = 0; j < 52; j++)
 		{
 			if (a[j] == *(s + i))
-				{
-					*(s + i) = b[j];
-					break;
-				}
+			{
+				*(s + i) = b[j];
+				break;
+			}
 		}
 	}
 	return (s);
